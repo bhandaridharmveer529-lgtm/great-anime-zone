20fee0b2a80c914c9dcf3f8fe131f7e5c0c5fa34
bhandaridharmveer529-lgtm <bhandaridharmveer529@gmail.com>
2026-09-20 16:55:22 +0530

    Update animeData.js

--- animeData.js
+++ animeData.js
@@ -1,26 +1,195 @@
-const allAnimeData = {
-    "The Elusive Samurai": {
-        "title": "The Elusive Samurai",
-        "banner": "banners/the_elusive_samurai.jpg",
-        "lang": "Hindi Dubbed",
-        "latest_badge": "EP 1 Added",
-        "updated_at": "2026-09-20 06:03:58",
-        "seasons": {
-            "s1": {
-                "seasonName": "Season 1",
-                "seasonZip": "#",
-                "episodes": [
-                    {
-                        "ep": "Ep 1",
-                        "title": "T.E.S. Ep1. 720P",
-                        "watch": [
-                            "https://morencius.com/embed/working_fallback_link",
-                            "https://streamtape.com/e/backup_sample"
-                        ],
-                        "download": "https://morencius.com/embed/working_fallback_link"
-                    }
+const animeDatabase = [
+    {
+        id: 1,
+        name: "Naruto",
+        title: "Naruto",
+        img: "Naruto.jpg",
+        banner: "Naruto.jpg",
+        sub: "Hindi Dubbed",
+        lang: "Hindi Dubbed",
+        rating: "8.8",
+        year: "2002",
+        eps: "220",
+        genres: ["Action", "Adventure"],
+        isNew: false,
+        isTrending: true,
+        latestUpdate: "Completed",
+        desc: "Naruto Uzumaki, a noisy adolescent ninja, constantly searches for approval and recognition.",
+        seasons: {
+            s1: {
+                seasonName: "Season 1",
+                seasonZip: "https://terabox.com/s/naruto_season1_zip",
+                episodes: [
+                    { ep: 1, title: "Enter: Naruto Uzumaki!", link: "https://vidsrc.to/embed/anime/naruto/1", download: "#" },
+                    { ep: 2, title: "My Name is Konohamaru!", link: "https://vidsrc.to/embed/anime/naruto/2", download: "#" }
+                ]
+            }
+        }
+    },
+    {
+        id: 2,
+        name: "Solo Leveling",
+        title: "Solo Leveling",
+        img: "Solo Leveling.jpg",
+        banner: "Solo Leveling.jpg",
+        sub: "Hindi Dubbed",
+        lang: "Hindi Dubbed",
+        rating: "9.1",
+        year: "2024",
+        eps: "24",
+        genres: ["Action", "Fantasy"],
+        isNew: true,
+        isTrending: true,
+        latestUpdate: "S2 • Ep 12 Added",
+        desc: "In a world where hunters battle deadly monsters, a weak hunter gets a mysterious system.",
+        seasons: {
+            s1: {
+                seasonName: "Season 1",
+                seasonZip: [
+                    { name: "📥 Download Ep 1-6 Zip", link: "https://hubcloud.cx/drive/hcrhyacsn4rm444" },
+                    { name: "📥 Download Ep 7-12 Zip", link: "https://hubcloud.cx/drive/73t1iahey3i93yg" }
+                ],
+                episodes: [
+                    { ep: 1, title: "I'm Used to It", link: "https://streamtape.com/e/7RWQ13pyWDFoGe/S1E01_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv", download: "https://streamtape.com/v/7RWQ13pyWDFoGe/S1E01_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv" },
+                    { ep: 2, title: "If I Had One More Chance", link: "https://streamtape.com/e/zwpaKKo6MWiYrZr/S1E02_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv", download: "https://streamtape.com/v/zwpaKKo6MWiYrZr/S1E02_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv" },
+                    { ep: 3, title: "It's Like a Dungeon", link: "https://streamtape.com/e/w4VXz7v4Q3HlvM/S1E03_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv", download: "https://streamtape.com/v/w4VXz7v4Q3HlvM/S1E03_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv" },
+                    { ep: 4, title: "I've Gotta Get Stronger", link: "https://streamtape.com/e/opKKWwrkjgcJL78/S1E04_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv", download: "https://streamtape.com/e/opKKWwrkjgcJL78/S1E04_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv" },
+                    { ep: 5, title: "A Pretty Good Deal", link: "https://streamtape.com/e/bPaRG6RWLPCPjV9/S1E05_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv", download: "https://streamtape.com/v/bPaRG6RWLPCPjV9/S1E05_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv" },
+                    { ep: 6, title: "The Real Hunt Begins", link: "https://streamtape.com/e/9k3jLJ2J62uabD4/S1E06_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv", download: "https://streamtape.com/v/9k3jLJ2J62uabD4/S1E06_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv" },
+                    { ep: 7, title: "Let's See How Far I Can Go", link: "https://streamtape.com/e/wJR9lOaj0QtJwVV/S1E07_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv", download: "https://streamtape.com/v/wJR9lOaj0QtJwVV/S1E07_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv" },
+                    { ep: 8, title: "This Is Frustrating", link: "https://streamtape.com/e/xX4L7drqWPfj6y/S1E08_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv", download: "https://streamtape.com/v/xX4L7drqWPfj6y/S1E08_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv" },
+                    { ep: 9, title: "You've Been Hiding Your Skills", link: "https://streamtape.com/e/zDMD3Wqa14hYVdm/S1E09_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv", download: "https://streamtape.com/v/zDMD3Wqa14hYVdm/S1E09_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv" },
+                    { ep: 10, title: "What Is This, a Picnic?", link: "https://streamtape.com/e/R97k0mxL1jT9ZJ/S1E10_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv", download: "https://streamtape.com/v/R97k0mxL1jT9ZJ/S1E10_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv" },
+                    { ep: 11, title: "A Knight Who Defends an Empty Throne", link: "https://streamtape.com/e/d73D7B71A1IkkL4/S1E11_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv", download: "https://streamtape.com/v/d73D7B71A1IkkL4/S1E11_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv" },
+                    { ep: 12, title: "Arise", link: "https://streamtape.com/e/3WDkoAAVx9sdWAq/S1E12_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv", download: "https://streamtape.com/v/3WDkoAAVx9sdWAq/S1E12_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.in.mkv" }
+                ]
+            },
+            s2: {
+                seasonName: "Season 2",
+                seasonZip: "https://terabox.com/s/solo_season2_zip",
+                episodes: [
+                    { ep: 1, title: "You Aren't E-Rank Anymore", link: "https://streamtape.com/e/3DoLYvwqLDS3g8/S2E01_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv", download: "https://streamtape.com/v/3DoLYvwqLDS3g8/S2E01_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv" },
+                    { ep: 2, title: "I Need a Change", link: "https://streamtape.com/e/LkzjRGv9dXSyP7/S2E02_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv", download: "https://streamtape.com/v/LkzjRGv9dXSyP7/S2E02_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv" },
+                    { ep: 3, title: "It's Going to Get Busy", link: "https://streamtape.com/e/9yz197IAZ3tak1Q/S2E03_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv", download: "https://streamtape.com/v/9yz197IAZ3tak1Q/S2E03_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv" },
+                    { ep: 4, title: "Gotta Keep Moving", link: "https://streamtape.com/e/LdzgDZxIZ7fRleG/S2E04_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv", download: "https://streamtape.com/v/LdzgDZxIZ7fRleG/S2E04_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv" },
+                    { ep: 5, title: "A New Raid", link: "https://streamtape.com/e/Djw3evMaBQFkgbD/S2E05_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv", download: "https://streamtape.com/v/Djw3evMaBQFkgbD/S2E05_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv" },
+                    { ep: 6, title: "Red Gate Emergency", link: "https://streamtape.com/e/1RgKPz0ebaTeW3z/S2E06_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv", download: "https://streamtape.com/v/1RgKPz0ebaTeW3z/S2E06_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv" },
+                    { ep: 7, title: "Unseen Danger", link: "https://streamtape.com/e/KwpKrorW64Fz4k/S2E07_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv", download: "https://streamtape.com/v/KwpKrorW64Fz4k/S2E07_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv" },
+                    { ep: 8, title: "The True Power", link: "https://streamtape.com/e/QrwwJ0bebglg1y/S2E08_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv", download: "https://streamtape.com/v/QrwwJ0bebglg1y/S2E08_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv" },
+                    { ep: 9, title: "Shadow Army", link: "https://streamtape.com/e/wdKxAYMkvblJJW6/S2E09_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv", download: "https://streamtape.com/v/wdKxAYMkvblJJW6/S2E09_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv" },
+                    { ep: 10, title: "Jeju Raid Begins", link: "https://streamtape.com/e/w6YOPo0wPJuo0a/S2E010_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv", download: "https://streamtape.com/v/w6YOPo0wPJuo0a/S2E010_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv" },
+                    { ep: 11, title: "Clash of Kings", link: "https://streamtape.com/e/0Doq3emm39hbbvY/S2E011_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv", download: "https://streamtape.com/v/0Doq3emm39hbbvY/S2E011_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv" },
+                    { ep: 12, title: "Arise Again", link: "https://streamtape.com/e/oDMLwOqb4wljxj/S2E012_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv", download: "https://streamtape.com/v/oDMLwOqb4wljxj/S2E012_-_1080p_-_Solo_Leveling_-_HindiDubbed4u.In.mkv" }
+                ]
+            }
+        }
+    },
+    {
+        id: 3,
+        name: "Overlord",
+        title: "Overlord",
+        img: "Overlord.jpg",
+        banner: "Overlord.jpg",
+        sub: "Hindi Dubbed",
+        lang: "Hindi Dubbed",
+        rating: "8.5",
+        year: "2015",
+        eps: "13",
+        genres: ["Fantasy"],
+        isNew: false,
+        isTrending: false,
+        latestUpdate: "S1 • Ep 13 Added",
+        desc: "A player finds himself unable to log out from a fantasy MMORPG game.",
+        seasons: {
+            s1: {
+                seasonName: "Season 1",
+                seasonZip: "https://hubcloud.cx/drive/gafg7gluzpgpblc",
+                episodes: [
+                    { ep: 1, title: "End and Beginning", link: "https://streamtape.com/e/2DJ2wbbpKefZl6z/", download: "https://streamtape.com/v/2DJ2wbbpKefZl6z/" },
+                    { ep: 2, title: "Floor Guardians", link: "https://streamtape.com/e/XdpMAMDrpySZQd/", download: "https://streamtape.com/v/XdpMAMDrpySZQd/" },
+                    { ep: 3, title: "Battle of Carne Village", link: "https://streamtape.com/e/YWLwOrRewaSv990/", download: "https://streamtape.com/v/YWLwOrRewaSv990/" },
+                    { ep: 4, title: "Ruler of Death", link: "https://streamtape.com/e/7K7m2Ykol0uRaq/", download: "https://streamtape.com/v/7K7m2Ykol0uRaq/" },
+                    { ep: 5, title: "Two Leaders", link: "https://streamtape.com/e/6eYMpz0dbwl9r9k/", download: "https://streamtape.com/v/6eYMpz0dbwl9r9k/" },
+                    { ep: 6, title: "Journey", link: "https://streamtape.com/e/plWMG9dyKyTrQzA/", download: "https://streamtape.com/v/plWMG9dyKyTrQzA/" },
+                    { ep: 7, title: "Wise King of Forest", link: "https://streamtape.com/e/DQ63rX9ep8sd1r/", download: "https://streamtape.com/v/DQ63rX9ep8sd1r/" },
+                    { ep: 8, title: "Twin Swords of Slashing Light", link: "https://streamtape.com/e/8vdqL0Wr22Fowgk/", download: "https://streamtape.com/v/8vdqL0Wr22Fowgk/" },
+                    { ep: 9, title: "Dark Warrior", link: "https://streamtape.com/e/jVmV0zddQoCamM/", download: "https://streamtape.com/v/jVmV0zddQoCamM/" },
+                    { ep: 10, title: "True Vampire", link: "https://streamtape.com/e/K4Wm6Ma2rec0RRB/", download: "https://streamtape.com/v/K4Wm6Ma2rec0RRB/" },
+                    { ep: 11, title: "Confusion and Understanding", link: "https://streamtape.com/e/903VLBqq0Zua49W/", download: "https://streamtape.com/v/903VLBqq0Zua49W/" },
+                    { ep: 12, title: "Bloody Valkyrie", link: "https://streamtape.com/e/qMLAv0DWl3FzvgJ/", download: "https://streamtape.com/v/qMLAv0DWl3FzvgJ/" },
+                    { ep: 13, title: "Player VS Non Player Character", link: "https://streamtape.com/e/A1bBJwWqk9IZoA/", download: "https://streamtape.com/v/A1bBJwWqk9IZoA/" }
+                ]
+            }
+        }
+    },
+    {
+        id: 4,
+        name: "The Elusive Samurai",
+        title: "The Elusive Samurai",
+        img: "The Elusive Samurai.jpg",
+        banner: "The Elusive Samurai.jpg",
+        sub: "Hindi Dubbed",
+        lang: "Hindi Dubbed",
+        rating: "8.1",
+        year: "2024",
+        eps: "12",
+        genres: ["Action", "Adventure"],
+        isNew: true,
+        isTrending: true,
+        latestUpdate: "S1 • Ep 12 Added",
+        desc: "A runaway heir and a town of secret warriors build the perfect revenge story.",
+        seasons: {
+            s1: {
+                seasonName: "Season 1",
+                seasonZip: "https://terabox.com/s/samurai_season1_zip",
+                episodes: [
+                    { ep: 1, title: "May 22", link: "https://morencius.com/embed/0u2nsg1qq574", download: "https://morencius.com/file/0u2nsg1qq574" },
+                    { ep: 2, title: "Divine Child", link: "https://morencius.com/embed/yv3lx32h0isg", download: "https://morencius.com/file/yv3lx32h0isg" },
+                    { ep: 3, title: "Hide-and-Seek in Kamakura", link: "https://morencius.com/embed/sd7rczh43fn4", download: "https://morencius.com/file/sd7rczh43fn4" },
+                    { ep: 4, title: "Sadism", link: "https://morencius.com/embed/hf4b2424z7ep", download: "https://morencius.com/file/hf4b2424z7ep" },
+                    { ep: 5, title: "Dog-Shooting", link: "https://morencius.com/embed/if85zhj52ckj", download: "https://morencius.com/file/if85zhj52ckj" },
+                    { ep: 6, title: "Steal the Order", link: "https://morencius.com/embed/0ztwl68j8vw9", download: "https://morencius.com/file/0ztwl68j8vw9" },
+                    { ep: 7, title: "Winter in Shinano", link: "https://morencius.com/embed/fgoxfg2k1pob", download: "https://morencius.com/file/fgoxfg2k1pob" },
+                    { ep: 8, title: "Hide-and-Seek", link: "https://morencius.com/embed/hryr33a9ueti", download: "https://morencius.com/file/hryr33a9ueti" },
+                    { ep: 9, title: "My Buddha", link: "https://morencius.com/embed/ios6iiim7mhs", download: "https://morencius.com/file/ios6iiim7mhs" },
+                    { ep: 10, title: "Monster Child", link: "https://morencius.com/embed/urvly0eo25xu", download: "https://morencius.com/file/urvly0eo25xu" },
+                    { ep: 11, title: "Deputy Governor", link: "https://morencius.com/embed/acqr6k4j5qn7", download: "https://morencius.com/file/acqr6k4j5qn7" },
+                    { ep: 12, title: "The Elusive Samurai", link: "https://morencius.com/embed/5z3q223x7l9b", download: "https://morencius.com/file/5z3q223x7l9b" }
                 ]
             }
         }
     }
-};
+];
+
+const heroSlides = [
+    {
+        id: 4,
+        bg: "The Elusive Samurai.jpg",
+        tag: "FEATURED THIS WEEK",
+        title: "The Elusive Samurai",
+        desc: "A runaway heir. A town of secret warriors. The perfect revenge story for your next late-night binge.",
+        rating: "8.1",
+        eps: "12 Episodes",
+        age: "16+"
+    },
+    {
+        id: 2,
+        bg: "Solo Leveling.jpg",
+        tag: "TRENDING ACTION",
+        title: "Solo Leveling",
+        desc: "A weak hunter gets a mysterious system that grants him incredible powers.",
+        rating: "9.1",
+        eps: "24 Episodes",
+        age: "16+"
+    },
+    {
+        id: 1,
+        bg: "Naruto.jpg",
+        tag: "FAN FAVORITE",
+        title: "Naruto",
+        desc: "Naruto Uzumaki constantly searches for approval and recognition.",
+        rating: "8.8",
+        eps: "220 Episodes",
+        age: "13+"
+    }
+];
